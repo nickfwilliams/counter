55d8058f118cb5183bfcead54ecf8afe3441927d
Nick Williams <nick.williams@bristol.ac.uk>
2024-01-26 15:48:56 +0000

    Rewrite solution to help memorise learning

--- setup/app.js
+++ setup/app.js
@@ -1,13 +1,16 @@
-// Set initial count
+// set initial count
 let count = 0;
 
-// Select value and buttons
+// select value and buttons
 const value = document.querySelector("#value");
 const btns = document.querySelectorAll(".btn");
 
+// Loop over buttons using forEach and button event(e)
 btns.forEach(function (btn) {
 	btn.addEventListener("click", function (e) {
+		// declare styles and call currentTarget to classlist
 		const styles = e.currentTarget.classList;
+		// If statements to set value
 		if (styles.contains("decrease")) {
 			count--;
 		} else if (styles.contains("increase")) {
@@ -15,17 +18,18 @@ btns.forEach(function (btn) {
 		} else {
 			count = 0;
 		}
-
-		if (count > 0) {
-			value.style.color = "green";
-		}
+		// Set value colors
 		if (count < 0) {
 			value.style.color = "red";
+		}
+		if (count > 0) {
+			value.style.color = "green";
 		}
 		if (count === 0) {
 			value.style.color = "black";
 		}
 
+		//Set value text to count
 		value.textContent = count;
 	});
 });
